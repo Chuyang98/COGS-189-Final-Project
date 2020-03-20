ad2b3504fb0ede96bf6ed870d8821521c5d5f625
Chuyang98 <49204297+Chuyang98@users.noreply.github.com>
2020-03-20 16:03:49 -0700

    Add files via upload

--- powercalculation.m
+++ powercalculation.m
@@ -4,10 +4,9 @@ function [power,filtered] = powercalculation (N, fs, band, x)
     length = size(x,1)
     for i = 1:N
         signal = x(:,i)
-        fftsignal = fft(signal)
-        filtered_signal = bandpass(fftsignal,band,fs)
+        filtered_signal = bandpass(signal,band,fs)
         power(i) = sum(abs(filtered_signal).^2)
-        filtered(:,i) = abs(filtered_signal)
+        filtered(:,i) = abs(filtered_signal).^2
     end
     power(i) = power(i)/length
 end
